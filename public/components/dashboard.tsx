532bf5e42dfda02ab1fed20d20e0fca4297f6b18
Ubuntu <ubuntu@ip-172-31-65-230.ec2.internal>
2020-04-17 20:14:04 +0000

    hard coded temporary solution for resizing

--- public/components/dashboard.tsx
+++ public/components/dashboard.tsx
@@ -8,27 +8,39 @@ import ReactResizeDetector from 'react-resize-detector';
 class Dashboard extends React.Component<{}, any> {
   constructor(props) {
     super(props);
-    this.child = React.createRef();
+    this.children = [React.createRef(),React.createRef(),React.createRef()];
     this.state = {
-      data: [
-      ]
+      children: [],
+      count: 0
     };
   }
-  
-  onResize = () => {
-    this.child.current.autoResize();
-  };
-  
+
   render() {
     return (
       <Fragment>
+        {/* <GridLayout className="layout" cols={12} rowHeight={30} width={1200}>
+          {['a', 'b', 'c'].map((id, idx) => {
+            { this.state.children.push(React.createRef()) }
+            <div key={id} data-grid={{ x: 0, y: 4 * idx, w: 4, h: 10 }}>
+              <Plt ref={this.state.children[idx]} />
+              <ReactResizeDetector handleWidth handleHeight onResize={() => this.state.children[idx].current.autoResize()} />
+            </div>
+          })}
+        </GridLayout> */}
+
         <GridLayout className="layout" cols={12} rowHeight={30} width={1200}>
           <div key="a" data-grid={{ x: 0, y: 0, w: 4, h: 10 }}>
-            <Plt ref={this.child}/>
-            <ReactResizeDetector handleWidth handleHeight onResize={this.onResize} />
+            <Plt ref={this.children[0]} />
+            <ReactResizeDetector handleWidth handleHeight onResize={() => this.children[0].current.autoResize()} />
           </div>
-          <div key="b" data-grid={{ x: 4, y: 0, w: 4, h: 10 }}><Plt /></div>
-          <div key="c" data-grid={{ x: 9, y: 0, w: 4, h: 10 }}><Plt /></div>
+          <div key="b" data-grid={{ x: 4, y: 0, w: 4, h: 10 }}>
+            <Plt ref={this.children[1]} />
+            <ReactResizeDetector handleWidth handleHeight onResize={() => this.children[1].current.autoResize()} />
+          </div>
+          <div key="c" data-grid={{ x: 9, y: 0, w: 4, h: 10 }}>
+            <Plt ref={this.children[2]} />
+            <ReactResizeDetector handleWidth handleHeight onResize={() => this.children[2].current.autoResize()} />
+          </div>
         </GridLayout>
       </Fragment>
     );
