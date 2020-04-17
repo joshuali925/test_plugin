e3cf4eba41defd8dac80ce7fb365489d181475eb
Ubuntu <ubuntu@ip-172-31-65-230.ec2.internal>
2020-04-17 21:49:28 +0000

    add click annotations

--- public/components/dashboard.tsx
+++ public/components/dashboard.tsx
@@ -8,40 +8,40 @@ import ReactResizeDetector from 'react-resize-detector';
 class Dashboard extends React.Component<{}, any> {
   constructor(props) {
     super(props);
-    this.children = [React.createRef(),React.createRef(),React.createRef()];
     this.state = {
       children: [],
-      count: 0
+      data: [
+        {
+          id: 'a',
+          y: Array.from({ length: 15 }, () => Math.random() * 15),
+        },
+        {
+          id: 'b',
+          y: Array.from({ length: 15 }, () => Math.random() * 15),
+        },
+        {
+          id: 'c',
+          y: Array.from({ length: 15 }, () => Math.random() * 15),
+        },
+      ]
     };
   }
 
   render() {
+    this.state.data.forEach(element => {
+      this.state.children.push(React.createRef())
+    });
     return (
       <Fragment>
-        {/* <GridLayout className="layout" cols={12} rowHeight={30} width={1200}>
-          {['a', 'b', 'c'].map((id, idx) => {
-            { this.state.children.push(React.createRef()) }
-            <div key={id} data-grid={{ x: 0, y: 4 * idx, w: 4, h: 10 }}>
-              <Plt ref={this.state.children[idx]} />
+        <GridLayout className="layout" cols={12} rowHeight={30} width={1200}>
+          {this.state.data.map(({ id, y, annotations }, idx) => (
+            <div key={id} data-grid={{ x: 4 * idx, y: 0, w: 4, h: 10 }}>
+              <Plt ref={this.state.children[idx]} y={y} />
               <ReactResizeDetector handleWidth handleHeight onResize={() => this.state.children[idx].current.autoResize()} />
             </div>
-          })}
-        </GridLayout> */}
-
-        <GridLayout className="layout" cols={12} rowHeight={30} width={1200}>
-          <div key="a" data-grid={{ x: 0, y: 0, w: 4, h: 10 }}>
-            <Plt ref={this.children[0]} />
-            <ReactResizeDetector handleWidth handleHeight onResize={() => this.children[0].current.autoResize()} />
-          </div>
-          <div key="b" data-grid={{ x: 4, y: 0, w: 4, h: 10 }}>
-            <Plt ref={this.children[1]} />
-            <ReactResizeDetector handleWidth handleHeight onResize={() => this.children[1].current.autoResize()} />
-          </div>
-          <div key="c" data-grid={{ x: 9, y: 0, w: 4, h: 10 }}>
-            <Plt ref={this.children[2]} />
-            <ReactResizeDetector handleWidth handleHeight onResize={() => this.children[2].current.autoResize()} />
-          </div>
+          ))}
         </GridLayout>
+
       </Fragment>
     );
   }
--- public/components/visualization/plt.tsx
+++ public/components/visualization/plt.tsx
@@ -16,15 +16,46 @@ class Plt extends React.Component<{}, any> {
           marker: { color: 'red' },
           // text: 'hello'
         },
-      ]
+      ],
+      annotations: [],
     };
   }
-  
+
   autoResize() {
     Plotly.Plots.resize(this.graphDiv)
   }
-  
+
+  addAnnotation(e) {
+    let anno = {
+      x: e.points[0].x,
+      y: e.points[0].y,
+      xref: 'x',
+      yref: 'y',
+      text: 'Clicked',
+      showarrow: true,
+      arrowhead: 7,
+      ax: -40,
+      ay: -40,
+    }
+    this.state.annotations.push(anno)
+    Plotly.redraw(this.graphDiv)
+  }
+
   render() {
+    const peak_annotation = [
+      {
+        x: this.state.data[0].y.indexOf(Math.max(...this.state.data[0].y)) + 1,
+        y: Math.max(...this.state.data[0].y),
+        xref: 'x',
+        yref: 'y',
+        text: 'Peak',
+        showarrow: true,
+        arrowhead: 7,
+        ax: -40,
+        ay: -40
+      }
+    ];
+
     return (
       <Plot
         data={this.state.data}
@@ -33,31 +64,13 @@ class Plt extends React.Component<{}, any> {
         useResizeHandler={true}
         onInitialized={(figure, graphDiv) => this.graphDiv = graphDiv}
         onHover={() => Plotly.Plots.resize(this.graphDiv)}
+        onClick={(e) => this.addAnnotation(e)}
         layout={{
           height: this.props.height,
           width: this.props.width,
           autosize: true,
           title: this.props.title,
-          // plot_bgcolor: "#1d1e24",
-          // paper_bgcolor: "#1d1e24",
-          // font: {
-          //   color: "#dfe5ef"
-          // },
-          // plot_bgcolor: "#fafbfd",
-          // paper_bgcolor: "#fafbfd",
-          annotations: [
-            {
-              x: this.state.data[0].y.indexOf(Math.max(...this.state.data[0].y)) + 1,
-              y: Math.max(...this.state.data[0].y),
-              xref: 'x',
-              yref: 'y',
-              text: 'Peak',
-              showarrow: true,
-              arrowhead: 7,
-              ax: -40,
-              ay: -40
-            }
-          ]
+          annotations: this.state.annotations,
         }}
       />
     );
